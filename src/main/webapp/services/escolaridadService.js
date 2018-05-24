33c2cf25b878b91abc55c544340e1bbb5cd19dd2
glujano <glujan@sevenbrains.com.mx>
2018-05-23 22:46:12 -0500

    Restructuracion de proyecto para template y cambio de motor de base de datos a posgrest

---
+++ src/main/webapp/services/escolaridadService.js
@@ -0,0 +1,45 @@
+
+angular.module("modBuscadorService",["ngResource"])
+    .factory("EscolaridadRest", ["$resource",
+        function($resource){
+            return $resource("../Rest/escolaridad/:methodName", {}, {
+                put: {
+                    method:"POST"
+                    ,params:{
+                        methodName:"put"
+                        ,escolaridad:"@especialidad"
+                    }
+                    ,isArray: false
+                },
+                list: {
+                    method: "POST"
+                    ,params: {
+                        methodName: "list"
+                    }
+                    ,isArray: true
+                },
+                buscar: {
+                    method: "POST"
+                    ,params: {
+                        methodName: "buscar"
+                        ,busqueda: "@busqueda"
+                    }
+                    ,isArray: true
+                },
+                delete:{
+                    method:"POST"
+                    ,params:{
+                        methodName:"delete",
+                        escolaridad:"@escolaridad"
+                    }
+                },
+                id: {
+                    method:"GET"
+                    ,params:{
+                        methodName:"id"
+                        ,id:"id"
+                    }
+                    ,isArray: false
+                }
+            });
+        }]);
